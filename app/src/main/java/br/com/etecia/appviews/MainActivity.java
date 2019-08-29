aaced8013f4bf74001855cb2b7f1c7fd131fa42c
christian <christian.nascimento3@etec.sp.gov.br>
2019-08-28 21:42:19 -0300

    Initial commit

---
+++ app/src/main/java/br/com/etecia/appviews/MainActivity.java
@@ -0,0 +1,44 @@
+package br.com.etecia.appviews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText txtNum1, txtNum2;
+    Button btnSomar;
+    TextView lblResposta;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtNum1 = (EditText)findViewById(R.id.txtNum1);
+        txtNum2 = (EditText)findViewById(R.id.txtNum2);
+
+        //ação do botão somar
+        btnSomar = (Button)findViewById(R.id.btnSomar);
+
+        lblResposta = (TextView)findViewById(R.id.lblResultado);
+
+        btnSomar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                double resp = 0;
+
+                String txtN1 = txtNum1.getText().toString();
+                String txtN2 = txtNum2.getText().toString();
+
+                resp = Double.parseDouble(txtN1)+Double.parseDouble(txtN2);
+
+                lblResposta.setText(String.valueOf(resp));
+            }
+        });
+    }
+}
